326c6380c7e2806250e4300db55b79029074ab05
dev-shuvo <saifulshuvo2@gmail.com>
2024-03-18 01:57:07 +0600

    import bootstrap in app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import './bootstrap';
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import Home from './Home.vue';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+
 const app = createApp({});
 
 app.component('home-component', Home);
